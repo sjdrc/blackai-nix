4b4e8bf941aa93245e55b37eece0be1e2802f1cf
Sebastien Collier <sjdrc@users.noreply.github.com>
2025-08-31 23:22:02 +1000

    Remove vanta, kolide, and clamav

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*";
-
-    nur.url = "github:nix-community/NUR";
-
-    kolide-launcher.url = "github:kolide/nix-agent/main";
   };
 
   outputs = {
@@ -18,34 +14,12 @@
     pkgs = import nixpkgs {
       inherit system;
     };
-    nur = import inputs.nur {
-      inherit pkgs;
-      nurpkgs = pkgs;
-    };
   in {
     packages.openlens = pkgs.callPackage ./packages/openlens.nix {};
 
     nixosModules.blackai = {pkgs, ...}: {
-      imports = [
-        inputs.kolide-launcher.nixosModules.kolide-launcher
-        inputs.nur.modules.nixos.default
-        nur.repos.kokakiwi.modules.nixos.services.vanta-agent
-      ];
-
       # Enable vpn service
       services.tailscale.enable = true;
-
-      # Enable machine compliance tool
-      services.kolide-launcher.enable = true;
-
-      # Enable vanta agent
-      services.vanta-agent.enable = true;
-      services.vanta-agent.package = nur.repos.kokakiwi.vanta-agent;
-
-      # Enable antivirus
-      services.clamav.daemon.enable = true;
-      services.clamav.updater.enable = true;
-      services.clamav.scanner.enable = true;
 
       programs.direnv = {
         enable = true;
@@ -61,7 +35,6 @@
         kubectl
         stern # for @andrea-falco/lens-multi-pod-logs extension
         kubelogin-oidc
-        (pkgs.google-cloud-sdk.withExtraComponents [pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin])
       ];
     };
   };
